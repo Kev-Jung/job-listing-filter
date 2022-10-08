e76fc17aa22e0b0f9eaf89ec675b17a028da4f13
Kevin <jung.kevinj@gmail.com>
2022-10-08 10:51:06 -0700

    Add header background for mobile and desktop screens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
 import './App.scss';
+import HeaderBg from "./images/bg-header-mobile.svg"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div className='header-bg'>
+        <img className='header-bg-img' src={HeaderBg} alt="header-background"/>
+      </div>
+    
+
+    </>
   );
 }
 
